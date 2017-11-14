edbe980fddeb09946a4ade520f3d3099255cf7c3
Murderdoll <artem765@inbox.ru>
2017-11-14 15:53:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main() {
   CURL *curl = curl_easy_init();
   if(curl) {
     CURLcode res;
+    curl_easy_setopt(curl, CURLOPT_NOBODY, true);
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
     curl_easy_setopt(curl, CURLOPT_URL, "https://google.com/");
@@ -14,7 +15,7 @@ int main() {
     if(res == CURLE_OK) {
       long response_code;
       curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-      std::cout << "Responce code of https://vk.com/ "<< response_code << std::endl;
+      std::cout << "Responce code of https://google.com/ "<< response_code << std::endl;
     }
     curl_easy_cleanup(curl);
   }
