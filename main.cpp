dfbf9173d0631b9661bfe840b292d325f8cd979d
Murderdoll <artem765@inbox.ru>
2017-11-14 15:49:03 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,9 @@ int main() {
   CURL *curl = curl_easy_init();
   if(curl) {
     CURLcode res;
-    curl_easy_setopt(curl, CURLOPT_NOBODY, true);
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-    curl_easy_setopt(curl, CURLOPT_URL, "https://vk.com/");
+    curl_easy_setopt(curl, CURLOPT_URL, "https://google.com/");
     res = curl_easy_perform(curl);
     if(res == CURLE_OK) {
       long response_code;
