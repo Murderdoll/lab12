9d2e97b5b94bf910a7fbab24dbe108db735aec45
Murderdoll <artem765@inbox.ru>
2017-11-14 15:42:00 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,13 +8,12 @@ int main() {
   if(curl) {
     CURLcode res;
     curl_easy_setopt(curl, CURLOPT_NOBODY, true);
-    curl_easy_setopt(curl, CURLOPT_URL, "https://vk.com");
+    curl_easy_setopt(curl, CURLOPT_URL, "https://vk.com/");
     res = curl_easy_perform(curl);
-    std::cout << "FFF" << std::endl;
     if(res == CURLE_OK) {
       long response_code;
       curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-      std::cout << "Responce code of https://google.ru "<< response_code << std::endl;
+      std::cout << "Responce code of https://vk.com/ "<< response_code << std::endl;
     }
     curl_easy_cleanup(curl);
   }
